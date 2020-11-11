231d3bb318dfa6298dafd036671d28fe5bb9dea1
abmoger <57551169+abmoger@users.noreply.github.com>
2020-11-11 14:18:29 -0500

    Added project

---
+++ GOAP_Hospital/Assets/PatientCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Patient")
+        {
+            DialogueManager.scc.setGameStateValue("collision", "equals", "t");
+            Debug.Log(DialogueManager.scc.getSCCLine("Patient"));
+        }
+    }
+}
